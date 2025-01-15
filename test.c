433c6e5a266cd60cad30fd42b79bc66aebb17b4a
vichayabhas <156632600+vichayabhas@users.noreply.github.com>
2025-01-15 13:41:12 +0700

    Create test.c

---
+++ test.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+int main()
+{
+pid_t pid;
+char *av[] = {"cal","3","2021",(char *)0};
+pid = fork() ;
+if (pid < 0) {
+printf("Error : cannot fork\n");
+exit(1);
+}
+else if (pid == 0) {
+execvp("cal",av);
+}
+else {
+wait(NULL);
+return(0);
+}
+}
